e7ec334a7fc702cc87f9c46cec840d33afae5fc8
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2021-10-04 12:29:21 -0300

    Adicionando tela de marcar jogos

--- src/pages/game/FormNewGame.js
+++ src/pages/game/FormNewGame.js
@@ -8,6 +8,9 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import MenuItem from '@material-ui/core/MenuItem';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import { Checkbox } from '@material-ui/core';
 
 import { getOpponents } from './thunks';
 
@@ -19,44 +22,52 @@ export const FormNewGame = (props) => {
 
     return (
         <>
+        <FormGroup>
           <Grid container direction='row' >
                 <Grid item xs={6} >
                     <Box display="center" justifyContent="center" >
-                        <Box m={3} >
-                            <Typography variant='h5' textAlign='center'> Infos do jogo </Typography>
+                        <Box  m={2} p={1}>
+                            <Typography variant='h6' textAlign='center'> Modalidade </Typography>
+                            <FormControlLabel control={<Checkbox defaultChecked color="default" />} label="Futsal" />
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Campo" />
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Society" />
                         </Box>
-                        <Box  m={2}>
-                            <TextField label="X" variant="outlined" fullWidth/>
+                        <Box  m={2} p={1}>
+                            <Typography variant='h6' textAlign='center'> Tipo de mando de jogo </Typography>
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Casa" />
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Fora" />
                         </Box>
-                        <Box  m={2}>
-                            <TextField label="Y" variant="outlined" fullWidth/>
-                        </Box>
-                        <Box  m={2}>
-                            <TextField label="X" variant="outlined" fullWidth/>
-                        </Box>
-                        <Box  m={2}>
-                            <TextField label="Z" variant="outlined" fullWidth/>
-                        </Box>
-                        <Box m={2} display='flex' justifyContent='flex-end' >
-                            <Button variant="contained">Salvar</Button> 
+                        <Box  m={2} p={1}>
+                            <Typography variant='h6' textAlign='center'> Dia de jogo </Typography>
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Seg" />
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Ter" />
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Qua" />
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Qui" />
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Sex" />
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Sab" />
+                            <FormControlLabel control={<Checkbox color="default"/>} label="Dom" />
                         </Box>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
                     <Box display="center" justifyContent="center" >
-                        <Box m={3} >
-                            <Typography variant='h5' textAlign='center'> Oponentes </Typography>
-                        </Box>
                         <Box m={2} >
-                            <TextField id='oponente' variant="outlined" fullWidth select >
+                            <TextField id='oponente' variant="outlined" fullWidth select label='Oponentes'>
                                 {props.opponents.map(op => (
                                     <MenuItem key={op.id} value={op.nome}>{op.nome}</MenuItem>
                                 ))}
                             </TextField>
                         </Box>
+                        <Box m={2}>
+                            <TextField id='local' variant="outlined" fullWidth label='Local de Jogo'> Local</TextField>
+                        </Box>
+                        <Box m={2} display='flex' justifyContent='flex-end' >
+                            <Button variant="contained">Marcar</Button> 
+                        </Box>
                     </Box>
                 </Grid>
-            </Grid>  
+            </Grid>
+            </FormGroup>  
         </>
     )
 }
