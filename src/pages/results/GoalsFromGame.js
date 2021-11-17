2c78ce0ccecc2ac4e09e18d0b40c7c495b27780c
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2021-11-16 22:21:07 -0300

    Adicionando hook usada em sumula para pegar adversario

--- src/pages/results/GoalsFromGame.js
+++ src/pages/results/GoalsFromGame.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Typography, Grid, Box } from '@material-ui/core'
+import Button from '@material-ui/core/button';
+
+import useTeam from '../../hooks/useTeam'
 
 export const GoalsFromGame = (props) => {
+    var idOponente =  props.team.id === props.game.times[0] ? props.game.times[1] : props.game.times[0];
+    var timeOponente  = useTeam(idOponente);
+
     return (
         <Grid container direction='row' spacing={1}>
             <Grid item xs={1}/>
             <Grid item xs={10}>
                 <Box display='flex' justifyContent='center'>
-                    <Typography variant='h6'> Equipe - Equipe B </Typography> 
+                    <Typography variant='h6'> {props.summaryPerson === 'Mandante' ? props.team.nome : timeOponente.nome} x {props.summaryPerson === 'Visitante'? props.team.nome : timeOponente.nome} </Typography> 
                 </Box>
                 <Box display='flex' justifyContent='center'>
                     <Typography variant='h1'> 0 x 0 </Typography> 
@@ -20,7 +26,7 @@ export const GoalsFromGame = (props) => {
 }
 
 const mapStateToProps = (state) => ({
-    
+    team: state.team,
 })
 
 const mapDispatchToProps = {
